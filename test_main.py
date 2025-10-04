b7167557000dc8117b6862710ead6a9f6c1c1193
ronanrocking <ronanrocking@gmail.com>
2025-10-04 19:04:53 +0530

    3ema cluster working

--- test_main.py
+++ test_main.py
@@ -1,5 +1,11 @@
 import main.fetch
+import main.indicators
+import private.conditions
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import interp1d
 
-df = main.fetch.get_historical_candle(symbol="NBCC")
+# Fetch data
+df = main.fetch.get_all_candles("LICHSGFIN")
 
-print(df)
+print(list(private.conditions.ema_cluster_3(df, "past", 1)))
